116c8a4b3c732417b5e069ad67afdc0330e152a0
liuniu@smail.nju.edu.com <32408540+Niu-LIU@users.noreply.github.com>
2019-08-12 21:11:04 +0800

    made a comparison

--- progs/test.py
+++ progs/test.py
@@ -1,66 +1,23 @@
-from astropy.table import Table, Column
-from astropy import units as u
-from astropy.coordinates import SkyCoord
-import numpy as np
-import sys
+# icrf2-nga-09 - gaia dr2
+gli1 = w21[:3]
+rot1 = w21[3:6]
+qua1 = w21[6:]
 
-# My modules
-from my_progs.catalog.pos_err import pos_err_calc
+gerr1 = sig21[:3]
+rerr1 = sig21[3:6]
+qerr1 = sig21[6:]
 
-def read_icrf3(icrf3_file="../data/icrf3sx.txt"):
-    """Read the ICRF3 catalog.
+glimod1, glierr1 = vec_mod_calc(gli1, gerr1)
+rotmod1, roterr1 = vec_mod_calc(rot1, rerr1)
 
-    Parameter
-    ---------
-    icrf3_file : string
-        file name and path of the ICRF3 catalog
-    wv : string
-        wavelength, could be "sx", "k", or "xka".
+# icrf2-ga-09 - gaia dr2
+gli2 = w22[:3]
+rot2 = w22[3:6]
+qua2 = w22[6:]
 
-    Return
-    ------
-    icrf3 : an astropy.Table object
-        data in the catalog
-    """
+gerr2 = sig22[:3]
+rerr2 = sig22[3:6]
+qerr2 = sig22[6:]
 
-    icrf3 = Table.read(icrf3_file,
-                       format="ascii.fixed_width", data_start=16,
-                       names=["icrf_name", "iers_name", "type",
-                              "ra_err", "dec_err", "ra_dec_corr",
-                              "mean_obs", "beg_obs", "end_obs",
-                              "nb_sess", "nb_del"],
-                       col_starts=[5, 25, 35, 83, 98,
-                                   108, 118, 127, 136, 145, 150],
-                       col_ends=[20, 32, 35, 92, 106,
-                                 114, 124, 133, 142, 148, 155])
-
-    # Position information
-    ra_dec_str = Table.read(icrf3_file,
-                            format="ascii.fixed_width", data_start=16,
-                            names=["ra_dec"], col_starts=[40], col_ends=[77])
-
-
-    ra_dec = SkyCoord(ra_dec_str["ra_dec"], unit=(u.hourangle, u.deg))
-    ra = Column(ra_dec.ra, name="ra")
-    dec = Column(ra_dec.dec, name="dec")
-
-    # Add source position to the table
-    icrf3.add_columns([ra, dec], indexes=[3, 3])
-
-    # Add unit information
-    icrf3["ra_err"] = icrf3["ra_err"] * 15e3 * np.cos(ra_dec.dec.rad)
-    icrf3["ra_err"].unit = u.mas
-    icrf3["dec_err"].unit = u.arcsec
-    icrf3["dec_err"] = icrf3["dec_err"].to(u.mas)
-
-    # Calculate the semi-major axis of error ellipse
-    pos_err = pos_err_calc(
-        icrf3["ra_err"], icrf3["dec_err"], icrf3["ra_dec_corr"])
-
-    # Add the semi-major axis of error ellipse to the table
-    icrf3.add_column(pos_err, name="pos_err", index=9)
-
-    return icrf3
-
-if __name__ == '__main__':
-    read_icrf3()
+glimod2, glierr2 = vec_mod_calc(gli2, gerr2)
+rotmod2, roterr2 = vec_mod_calc(rot2, rerr2)
