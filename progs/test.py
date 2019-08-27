4e7ba8e0b1f75eb20e1c4758b91292c5cbbfde24
liuniu@smail.nju.edu.com <32408540+Niu-LIU@users.noreply.github.com>
2019-08-27 22:01:42 +0800

    Add new comparisons

--- progs/test.py
+++ progs/test.py
@@ -1,68 +1,24 @@
-# UT1
-fig, (ax0, ax1, ax2) = plt.subplots(figsize=(12, 12), sharex=True, nrows=3)
+fig, (ax0, ax1) = plt.subplots(figsize=(8, 8), nrows=2, sharex=True)
 
-# Error bar plot
-ax0.errorbar(pmrEpoch.jyear,
-             ngaeoboft["dut"],
-             yerr=ngaeoboft["dut_err"],
-             fmt="b.",
-             ms=1,
-             elinewidth=0.05,
-             ecolor="k",
-             errorevery=5)
+ax0.plot(soldif1["dec"], soldif1["dra"], ".", ms=1)
+ax1.plot(soldif1["dec"], soldif1["ddec"], ".", ms=1)
 
-# Offset
-ax1.plot(pmrEpoch.jyear, ngaeoboft["dut"], "b.", ms=1)
-epomed = bn.move_median(pmrEpoch.jyear, window=50)
-dutmed = bn.move_median(ngaeoboft["dut"], window=50)
-ax1.plot(epomed, dutmed, "r")
+ax0.set_ylabel("Offset in R.A. [mas]")
 
+ax1.set_xlabel("Declination [$^\circ$]")
+ax1.set_ylabel("Offset in decl. [mas]")
 
-# Formal error
-ax2.plot(pmrEpoch.jyear, ngaeoboft["dut_err"], "b.", ms=1)
-duterrmed = bn.move_median(ngaeoboft["dut_err"], window=50)
-ax2.plot(epomed, duterrmed, "r")
+ax1.set_xticks(np.arange(-90, 91, 30))
 
+ax0.set_xlim([-90, 90])
+ax0.set_ylim([-0.1, 0.1])
+ax1.set_ylim([-0.1, 0.1])
 
-# Add a horizontal line
-x = np.arange(1979, 2021, 0.5)
-y0 = np.zeros_like(x)
-ax0.plot(x, y0, "r", lw=1)
+ax0.set_title("Positional difference vs decl.", fontsize=15)
 
-# Add a fitted line
-# x0 = np.arange(1979, 2021, 1)
-# y0 = x0 * 0.2 - 403
-# ax.plot(x0, y0, "r", lw=2)
-
-ax2.set_yscale("log")
-
-# Limits
-ax0.axis([1979, 2020, -5, 5])
-ax1.set_ylim([-1, 1])
-# ax2.set_ylim([30, 40000])
-
-# Titles and Labels
-ax0.set_title("$UT1$ (icrf2ga $-$ icrf3ga)")
-ax0.set_ylabel("$\mu$s")
-ax1.set_ylabel("Offset [$\mu$s]")
-ax2.set_ylabel("Errors [$\mu$s]")
-ax2.set_xlabel("Year")
-
-# Ticks
 ax0.xaxis.set_ticks_position("both")
 ax0.yaxis.set_ticks_position("both")
-
 ax1.xaxis.set_ticks_position("both")
 ax1.yaxis.set_ticks_position("both")
 
-ax2.xaxis.set_ticks_position("both")
-ax2.yaxis.set_ticks_position("both")
-
-# Add minor ticks
-ax0.xaxis.set_minor_locator(MultipleLocator(1))
-ax1.yaxis.set_minor_locator(MultipleLocator(0.5))
-
-# Add grid
-# ax0.grid()
-ax1.grid()
-ax2.grid()
+plt.subplots_adjust(hspace=0.1)
