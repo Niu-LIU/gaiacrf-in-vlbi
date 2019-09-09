fa84d1588e9f818a851cb566f7550a87fcf56caf
Niu-LIU <liuniu@smail.nju.edu.cn>
2019-09-09 15:40:48 +0800

    update pos_err

--- progs/test.py
+++ progs/test.py
@@ -1,37 +1,9 @@
-# My modules
-from my_progs.catalog.vsh_deg1_cor import vsh_deg01_fitting
+comsou = join(k2sx, xka2sx, keys=["iers_name"], table_names=["k", "xka"])
+comsou = join(comsou, opt2sx, keys=["iers_name"])
 
-# Gaia DR2 - ICRF3 S/X
-pmra = np.array(defsou["pmra"])
-pmdec = np.array(defsou["pmdec"])
-pmraerr = np.array(defsou["pmra_err"])
-pmdecerr = np.array(defsou["pmdec_err"])
-pmradeccov = np.array(defsou["pmra_err"] * defsou["pmdec_err"] * defsou["pmra_pmdec_corr"])
-
-# Transformation parameters
-# l_max = 2
-par, sig, _, _, _, _ = vsh_deg01_fitting(
-    pmra, pmdec, rarad, decrad, pmraerr, pmdecerr,
-    cov=pmradeccov, elim_flag="None")
-
-# mas -> uas
-spin = par * 1.e3
-serr = sig * 1.e3
-
-# Print results
-print("Estimates (%6d sources)\n"
-      "----------------------------------------------"
-      "----------------------------------------------\n"
-      "                 Rotation [uas]                  "
-      "                 Glide [uas]               \n"
-      "             x             y             z"
-      "             x             y             z\n"
-      "----------------------------------------------"
-      "----------------------------------------------\n"
-      "        %+4.0f +/- %3.0f  %+4.0f +/- %3.0f  %+4.0f +/- %3.0f"
-      "        %+4.0f +/- %3.0f  %+4.0f +/- %3.0f  %+4.0f +/- %3.0f\n"
-      "----------------------------------------------"
-      "----------------------------------------------\n" %
-      (pmra.size,
-       spin[3], serr[3], spin[4], serr[4], spin[5], serr[5],
-       spin[0], serr[0], spin[1], serr[1], spin[2], serr[2],))
+# Rename some columns
+comsou.rename_columns(['dra', 'ddec', 'dra_err', 'ddec_err', 'dra_ddec_cov',
+                       'ang_sep', 'nor_ra', 'nor_dec', 'nor_sep'],
+                      ['dra_opt', 'ddec_opt', 'dra_err_opt', 'ddec_err_opt',
+                       'dra_ddec_cov_opt',
+                       'ang_sep_opt', 'nor_ra_opt', 'nor_dec_opt', 'nor_sep_opt'])
